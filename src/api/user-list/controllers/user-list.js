5c0c838d85775614457f467bdf55ac84368ad8ee
Kishan-Bisht28 <150129690+Kishan-Bisht28@users.noreply.github.com>
2023-12-11 16:34:31 +0530

    Delete src/api/user-list directory

--- src/api/user-list/controllers/user-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * user-list controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::user-list.user-list');
--- src/api/user-list/routes/user-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * user-list router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::user-list.user-list');
