84a5693f96bfed52f9a2c1538ea86ce0aad9dfdc
Krishna Vaibhav <krishna.vaibhav@yash.com>
2025-06-11 13:34:06 +0530

    Changes related to strapi social sign in table created for data storage

---
+++ src/api/custom-user-table/routes/custom-user-table.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * custom-user-table router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::custom-user-table.custom-user-table');
