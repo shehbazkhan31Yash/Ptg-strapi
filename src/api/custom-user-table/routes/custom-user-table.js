ec057d6824309618c8ec9f051b53dfb5df5f5f3d
Krishna Vaibhav <krishna.vaibhav@yash.com>
2025-06-13 17:53:51 +0530

    Changes related to misspell

--- src/api/custom-user-table/routes/custom-user-table.js
+++ src/api/custom-user-table/routes/custom-user-table.js
@@ -2,8 +2,8 @@ module.exports = {
   routes: [
     {
       method: "POST",
-      path: "/cutom-user-table/create-custom-user",
-      handler: "cutom-user-table.createCustomUser",
+      path: "/custom-user-table/create-custom-user",
+      handler: "custom-user-table.createCustomUser",
       config: {
         policies: [], // You can add role-based policies here
         auth: false, // Set to true if you want auth
