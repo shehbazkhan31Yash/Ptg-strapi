f5bc5394ad075cf443dd05b3961adec149e707c6
MD Faiyazul Islam <faiyazul.islam@yash.com>
2025-06-19 15:27:33 +0530

    fix for added best-practice-doc-strapi

---
+++ src/api/best-practice-document/services/best-practice-document.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * best-practice-document service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::best-practice-document.best-practice-document');
